05708836d39ea15f67848b9f319fb734d81ce052
LeeOkSu <counman2@nate.com>
2023-04-22 22:23:54 +0900

    frontend commit

---
+++ frontend/src/bootStarp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button, Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container>
+      <h1>Hello, React Bootstrap!</h1>
+      <Button variant="primary">Click me</Button>
+    </Container>
+  );
+}
+
+export default App;
