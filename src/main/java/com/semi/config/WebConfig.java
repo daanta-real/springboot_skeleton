1ba7a03a8a5695de4b28a346a25a74576853c0c5
Junsung Park / Daanta <daanta@naver.com>
2023-07-16 23:59:09 +0900

    230716_2359 Adding Lucy XSS dependency

--- src/main/java/com/semi/config/WebConfig.java
+++ src/main/java/com/semi/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.semi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,5 +16,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(false);
+    }
+
+    //Lucy Xss filter 적용
+    @Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> getFilterRegistrationBean(){
+        FilterRegistrationBean<XssEscapeServletFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new XssEscapeServletFilter());
+        registrationBean.setOrder(1);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
     }
 }
