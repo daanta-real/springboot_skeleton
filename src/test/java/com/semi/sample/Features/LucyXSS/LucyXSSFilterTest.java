1678bac35e7dbd2f3d01d0a11e27a36a8925ce08
Junsung Park / Daanta <daanta@naver.com>
2023-07-23 00:31:14 +0900

    230723_0031 Adding Lucy XSS dependency

--- src/test/java/com/semi/sample/Features/LucyXSS/LucyXSSFilterTest.java
+++ src/test/java/com/semi/sample/Features/LucyXSS/LucyXSSFilterTest.java
@@ -10,12 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Traditional Lucy XSS Filter usage practice
+ * Manual from: <a href="http://naver.github.io/lucy-xss-filter/kr/">...</a>
  */
 @Slf4j
 public class LucyXSSFilterTest {
 
     /**
-     * To handle just simple text, just use "XssPreventer".
+     * [1] PREVENTING HTML
+     * To totally prevent any HTML, just use "XssPreventer".
      * 1) .escape(): Escape all HTML chars for preventing applying HTML
      * 2) .unescape(): Make all HTML chars for applying HTML
      */
@@ -29,36 +31,41 @@ public class LucyXSSFilterTest {
     }
 
     /**
-     * To handle the complex HTML String, use "XssFilter".
+     * [2] FILTERING HTML
+     * To handle the complex HTML String but just prevent XSS attack, use "XssFilter".
      * XssFilter can handle two types: DOM(parse after whole xml has been loaded), SAX(parse line by line)
+     * Before using any of these, you must make "lucy-xss-superset-sax.xml" file at the classpath root directory.
+     * <p>
+     * 1) DOM
+     * String result = XssFilter.getInstance.doFilter(originalString)
+     * Before using, you must make "lucy-xss.xml" file at the classpath root directory.
      */
-
-    // 1) DOM
     @Test
     public void pairQuoteCheckOtherCase() {
         XssFilter filter = XssFilter.getInstance("lucy-xss-superset.xml");
         String dirty = "<img src=\"<img src=1\\ onerror=alert(1234)>\" onerror=\"alert('XSS')\">";
         String expected = "<img src=\"\"><!-- Not Allowed Attribute Filtered ( onerror=alert(1234)) --><img src=1\\>\" onerror=\"alert('XSS')\"&gt;";
-        String actual = filter.doFilter(dirty);
-        assertEquals(expected, actual);
+        String result = filter.doFilter(dirty);
+        assertEquals(expected, result);
 
         dirty = "<img src='<img src=1\\ onerror=alert(1234)>\" onerror=\"alert('XSS')\">";
         expected = "<img src=''><!-- Not Allowed Attribute Filtered ( onerror=alert(1234)) --><img src=1\\>\" onerror=\"alert('XSS')\"&gt;";
-        actual = filter.doFilter(dirty);
-        assertEquals(expected, actual);
+        result = filter.doFilter(dirty);
+        assertEquals(expected, result);
     }
 
     /**
      * 2) SAX = XssSaxFilter
-     * Get an instance and use .doFilter()
+     * String result = XssSaxFilter.getInstance.doFilter(originalString)
+     * Before using, you must make "lucy-xss-sax.xml" file at the classpath root directory.
      */
     @Test
     public void testSuperSetFix() {
         XssSaxFilter filter = XssSaxFilter.getInstance("lucy-xss-superset-sax.xml");
         String dirty = "<img src=\"<img src=1\\ onerror=alert(1234)>\" onerror=\"alert('XSS')\">";
         String expected = "<TABLE class=\"Naver_Layout_Main\" style=\"TABLE-LAYOUT: fixed\" cellSpacing=\"0\" cellPadding=\"0\" width=\"743\">" + "</TABLE>" + "<SPAN style=\"COLOR: #66cc99\"></SPAN>";
-        String actual = filter.doFilter(dirty);
-        assertEquals(expected, actual);
+        String result = filter.doFilter(dirty);
+        assertEquals(expected, result);
     }
 
 }
