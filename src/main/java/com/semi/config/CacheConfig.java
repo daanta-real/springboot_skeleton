2500248fbdebfbf870a386d1b2821de1263b4ff0
Junsung Park / Daanta <daanta@naver.com>
2023-12-24 23:54:50 +0900

    231224_2354 Java 21 / Gradle 8.5 / EhCache

--- src/main/java/com/semi/config/CacheConfig.java
+++ src/main/java/com/semi/config/CacheConfig.java
@@ -2,10 +2,9 @@ package com.semi.config;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @EnableCaching
@@ -13,14 +12,7 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new EhCacheCacheManager(ehCacheManagerFactory().getObject());
+        return new ConcurrentMapCacheManager("addresses");
     }
 
-    @Bean
-    public EhCacheManagerFactoryBean ehCacheManagerFactory() {
-        EhCacheManagerFactoryBean factoryBean = new EhCacheManagerFactoryBean();
-        factoryBean.setConfigLocation(new ClassPathResource("config/ehcache.xml"));
-        factoryBean.setShared(true);
-        return factoryBean;
-    }
 }
--- src/main/java/com/semi/main/MainController.java
+++ src/main/java/com/semi/main/MainController.java
@@ -2,11 +2,13 @@ package com.semi.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class mainController {
+@RequestMapping("/")
+public class MainController {
 
-	@GetMapping("/")
+	@GetMapping("")
 	public String index() {
 		return "index.html";
 	}
